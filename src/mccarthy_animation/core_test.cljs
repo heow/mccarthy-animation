7bdb8156fe37edbe091fef62eb0b3e6e60edc409
Heow Goodman <spamtrap@alphageeksinc.com>
2017-07-11 21:08:21 -0400

    roll in all the tests

--- src/mccarthy_animation/core_test.cljs
+++ src/mccarthy_animation/core_test.cljs
@@ -1,6 +1,8 @@
 (ns mccarthy-animation.core-test
   (:require [cljs.test :refer-macros [deftest is testing run-tests]] ))
 
-(deftest a-test (is (= 0 1)))
+;; so far we don't test core, this is the top-level, it's run
+;; TODO test it in phantom js?
+(deftest a-test (is (= 1 1)))
 
 (cljs.test/run-tests) ; run this from planck
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #lein cljsbuild test
 pushd ./src
+planck ./mccarthy_animation/core_test.cljs
 planck ./mccarthy_animation/lispm_test.cljs
-#planck ./mccarthy_animation/character_test.cljs
+planck ./mccarthy_animation/character_test.cljs
 popd
